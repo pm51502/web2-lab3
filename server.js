b3ab28a867e00190250e2cacf5dd98ae08d36741
Petar Milicevic <pm51502@fer.hr>
2022-01-05 21:48:45 +0100

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
-const serveStatic = require('serve-static')
+//const serveStatic = require('serve-static')
 const path = require('path')
+var cors = require("cors")
+const fs = require("fs");
 
 const app = express()
 
+app.use(express.static('dist'));
+app.use(cors());
+
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+//app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 //this * route is to serve project on different page routes except root `/`
 // app.get(/.*/, function (req, res) {
